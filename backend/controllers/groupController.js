e62de731a4c42905dae87c5b2c0b77e72ba659af
Buddyboy201 <aprakriya201@gmail.com>
2021-02-12 02:46:31 -0800

    Updated user/group controller/routers with essential boilerplate

---
+++ backend/controllers/groupController.js
@@ -0,0 +1,17 @@
+var Group = require("../models/groupModel");
+
+exports.group_get_data = function (req, res) {
+    res.send("GET REQUEST FOR GROUP DATA --BOILERPLATE STATEMENT--");
+};
+
+exports.group_post_data = function (req, res) {
+    res.send("POST REQUEST FOR GROUP DATA --BOILERPLATE STATEMENT--");
+};
+
+exports.group_delete_data = function (req, res) {
+    res.send("DELETE REQUEST FOR GROUP DATA --BOILERPLATE STATEMENT--");
+};
+
+exports.group_create = function (req, res) {
+    res.send("POST REQUEST FOR GROUP CREATION --BOILERPLATE STATEMENT--");
+};
