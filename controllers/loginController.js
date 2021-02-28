8cb0ef77056bc8dd430cf27270ef69629f75d668
Angad <angadmisra28@gmail.com>
2021-03-01 04:05:03 +0530

    Small loginController fix

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 exports.handleLogin = (req, res) => {
     User.findOne({ email: req.body.email }, async (err, user) => {
         if (err) throw err;
-        if (user !== null && user.validatePassword(req.body.password)) {
+        if (user !== null) {
             const valid = await user.validatePassword(req.body.password);
             if (valid) {
                 req.session.loggedIn = true;
